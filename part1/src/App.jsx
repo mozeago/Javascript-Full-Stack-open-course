70fd15fb91632af7f0a8b2621a61956651626e51
Moses Asiago <mozzeago@gmail.com>
2023-11-06 13:06:41 +0300

    cleaning part1, refactoring App.jsx and main.Jsx

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,35 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  // eslint-disable-next-line no-unused-vars
+  const course = 'Half Stack application development';
+  const part1 = 'Fundamentals of React ';
+  const exercises1 = 10;
+  const part2 = 'Using Props to pass data ';
+  const exercises2 = 7;
+  const part3 = 'State of a Component ';
+  const exercises3 = 14;
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+    <div>
+      <h1>{course}</h1>
+      <p>
+        {part1}
+        {exercises1}
       </p>
-    </>
-  )
+      <p>
+        {part2}
+        {exercises2}
+      </p>
+      <p>
+        {part3}
+        {exercises3}
+      </p>
+      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+    </div>
+  );
 }
 
-export default App
+export default App;
