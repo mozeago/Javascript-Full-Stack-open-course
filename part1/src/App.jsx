b67bf7edd4dee842f85823f743a6a3a2ca5914e6
Moses Asiago <mozzeago@gmail.com>
2023-11-15 17:31:13 +0300

    1.7: unicafe step2

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -4,15 +4,20 @@ const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  const [total, setTotal] = useState(0);
   const onSmashGood = () => {
     setGood(good + 1);
+    setTotal(total + 1);
   };
   const onSmashNeutral = () => {
     setNeutral(neutral + 1);
+    setTotal(total + 1);
   };
   const onSmashBad = () => {
     setBad(bad + 1);
+    setTotal(total + 1);
   };
+  const averagePositive = () => {};
   return (
     <>
       <div>
@@ -23,6 +28,9 @@ const App = () => {
         <p>Good {good}</p>
         <p>Neutral {neutral}</p>
         <p>Bad {bad}</p>
+        <p>All {total}</p>
+        <p>Average {total / 3}</p>
+        <p>Positive %</p>
       </div>
     </>
   );
